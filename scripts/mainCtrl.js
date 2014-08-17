ba6b8c370ce21395785523474246193b0a35e300
banezaklan <bzaklan@gmail.com>
2014-08-17 12:20:56 +0200

    Various changes

--- scripts/mainCtrl.js
+++ scripts/mainCtrl.js
@@ -9,17 +9,19 @@ app.controller('mainCtrl', function( $scope, $http ) {
         $scope.money = "100$";
         $scope.menuItemSelection = "";
         $scope.menuItems = [ 
-                {'id':1, label:'Home',url:'Welcome', selected:true,
-                    subItems: [
+                {'id':1, label:'Home',url:'Welcome', selected:true, subItems:[]
+                    /*subItems: [
                         {'id':11, label:'Option 1',url:'Welcome', selected:true},
                         {'id':12, label:'Option 2',url:'Welcome', selected:true},
                         {'id':13, label:'Option 3',url:'Welcome', selected:true}
-                    ]
+                    ]*/
                 },
+            /*
                 {'id':2, label:'Some Other',url:'SomeOther', selected:false, subItems: []},
                 {'id':3, label:'Link3',url:'Link3', selected:false, subItems: []},
                 {'id':4, label:'Link4',url:'Link4', selected:false, subItems: []},
                 {'id':5, label:'Link5',url:'Link5', selected:false, subItems: []},
+                */
             ];            
     };
 
--- server/server.js
+++ server/server.js
@@ -1,3 +1,34 @@
-/**
- * Created by bane on 10.8.14..
- */
+var express = require("express"),
+    app = express(),
+    bodyParser = require('body-parser')
+    errorHandler = require('errorhandler'),
+    methodOverride = require('method-override'),
+    port = parseInt(process.env.PORT, 10) || 8080;
+var path = require('path');
+
+var rootPath = '/home/bane/Development/angular/ng-basic';
+
+app.use(methodOverride());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+//initialize static server that will spit out contents of public folder
+app.use('/', express.static(rootPath));
+
+//send our main angular html file if any link without dot is requested, e.g. 'http://someurl/about'
+//this is our actual server side redirect, we don't send index.html when there's dot in link assuming such a request
+//is for static data like .js, .css or .html
+app.get('/[^\.]+$', function(req, res){
+    res.set('Content-Type', 'text/html');
+    //res.sendFile(rootPath/index.html');
+    res.sendFile(path.resolve(rootPath + '/index.html'));
+});
+
+app.use(errorHandler({
+  dumpExceptions: true,
+  showStack: true
+}));
+
+console.log("Simple static server listening at http://localhost:" + port);
+app.listen(port);
